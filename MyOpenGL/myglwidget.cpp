1af61acd04c33389251296082729d9148ed7fe19
davidmills <david.moulin@telecom-st-etienne.fr>
2017-05-14 17:53:53 +0200

    Fix + Target

--- MyOpenGL/myglwidget.cpp
+++ MyOpenGL/myglwidget.cpp
@@ -239,7 +239,7 @@ void MyGLWidget::draw()
 {
     glColor3f(1,1,1);
     //drawCube(204,102,0);
-    drawBase();
+    drawTarget();
     //drawSol();
     //drawCiel();
 }
@@ -250,10 +250,10 @@ void MyGLWidget::drawCube(int R,int G,int B)
     glBegin(GL_QUADS);
 
         glColor3ub(R, G, B); //face 1
-        glVertex3d(x,1,1);
-        glVertex3d(x,1,-1);
-        glVertex3d(-y,1,-1);
-        glVertex3d(-,1,1);
+        glVertex3d(1,1,1);
+        glVertex3d(1,1,-1);
+        glVertex3d(-1,1,-1);
+        glVertex3d(-1,1,1);
 
         glColor3ub(R, G, B); //face 2
         glVertex3d(1,-1,1);
@@ -315,5 +315,61 @@ void MyGLWidget::drawBase()
 }
 void MyGLWidget::drawTarget()
 {
+    const double PI = 3.14159;
+
+
+
+        /* top triangle */
+        double i, resolution  = 0.1;
+        double height = 0.3;
+        double radius = 0.5;
+
+
+        glPushMatrix();
+        glTranslatef(0, -0.5, 0);
+
+        glBegin(GL_TRIANGLE_FAN);
+            glTexCoord2f( 0.5, 0.5 );
+            glVertex3f(0, height, 0);  /* center */
+            for (i = 2 * PI; i >= 0; i -= resolution)
+
+            {
+                glTexCoord2f( 0.5f * cos(i) + 0.5f, 0.5f * sin(i) + 0.5f );
+                glVertex3f(radius * cos(i), height, radius * sin(i));
+            }
+            /* close the loop back to 0 degrees */
+            glTexCoord2f( 0.5, 0.5 );
+            glVertex3f(radius, height, 0);
+        glEnd();
+
+        /* bottom triangle: note: for is in reverse order */
+        glBegin(GL_TRIANGLE_FAN);
+            glTexCoord2f( 0.5, 0.5 );
+            glVertex3f(0, 0, 0);  /* center */
+            for (i = 0; i <= 2 * PI; i += resolution)
+            {
+                glTexCoord2f( 0.5f * cos(i) + 0.5f, 0.5f * sin(i) + 0.5f );
+                glVertex3f(radius * cos(i), 0, radius * sin(i));
+            }
+        glEnd();
+
+        /* middle tube */
+        glBegin(GL_QUAD_STRIP);
+            for (i = 0; i <= 2 * PI; i += resolution)
+            {
+                const float tc = ( i / (float)( 2 * PI ) );
+                glTexCoord2f( tc, 0.0 );
+                glVertex3f(radius * cos(i), 0, radius * sin(i));
+                glTexCoord2f( tc, 1.0 );
+                glVertex3f(radius * cos(i), height, radius * sin(i));
+            }
+            /* close the loop back to zero degrees */
+            glTexCoord2f( 0.0, 0.0 );
+            glVertex3f(radius, 0, 0);
+            glTexCoord2f( 0.0, 1.0 );
+            glVertex3f(radius, height, 0);
+        glEnd();
+
+        glPopMatrix();
 
 }
